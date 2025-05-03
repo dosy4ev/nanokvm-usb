fd4a5db759e6d2700b1234fb841130d50a76b67d
Peter Dosychev <dosy4ev@gmail.com>
2025-05-03 23:08:27 +0500

    add src directory

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "main",
-        .root_source_file = b.path("main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
